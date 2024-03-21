a001e56915e57f6d7af10c3416ff97eeee4a71ab
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-22 02:46:10 +0800

    cp4

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 from utils import in_specific_channel
+import time as t
 
 channel_name = 'general'
 
@@ -22,6 +23,13 @@ class General(commands.Cog):
     async def add(self, ctx: commands.Context, left: int, right: int):
         """Adds two numbers together."""
         await ctx.send(left + right)
+        
+    @in_specific_channel(channel_name)
+    @commands.command()
+    async def time(self, ctx: commands.Context):
+        """顯示時間"""
+        time_now = t.strftime("%Y年%m月%d日 %H點%M分", t.localtime())
+        await ctx.send(f'現在時間: {time_now}')
         
 def setup(bot: commands.Bot):
     bot.add_cog(General(bot))
