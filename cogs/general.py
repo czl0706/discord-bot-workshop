a7d5e9737780e4d4c06b455a297e8e7f32c6d770
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-22 03:50:56 +0800

    cp6

--- cogs/general.py
+++ cogs/general.py
@@ -1,13 +1,19 @@
 import nextcord
 from nextcord.ext import commands
 from utils import in_specific_channel
+
 import requests
+import random
+import json
 
 channel_name = 'general'
 
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # 讀入卡片資料
+        with open('./assets/cards.json', 'r', encoding='utf-8') as f:
+            self.decks = json.load(f)
         
     @in_specific_channel(channel_name)
     @commands.command()
@@ -23,6 +29,16 @@ class General(commands.Cog):
     async def add(self, ctx: commands.Context, left: int, right: int):
         """Adds two numbers together."""
         await ctx.send(left + right)
+    
+    @in_specific_channel(channel_name)
+    async def bonk(self, ctx: commands.Context):
+        card = random.choice(self.decks)
+        
+        title = card['title']
+        image_link = card['src']
+        
+        await ctx.send(f'{title}')
+        await ctx.send(f'{image_link}')
         
     def dog(self, ):
         response = requests.get('https://dog.ceo/api/breeds/image/random')
