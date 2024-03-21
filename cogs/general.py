55312b3baa5adaaec24587a24c7e93190a03ae5d
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-22 03:57:58 +0800

    cp6

--- cogs/general.py
+++ cogs/general.py
@@ -1,13 +1,19 @@
 import nextcord
 from nextcord.ext import commands
 from utils import in_specific_channel
-import time as t
+
+import requests
+import random
+import json
 
 channel_name = 'general'
 
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # 讀入卡片資料
+        ...:
+            self.decks = ...
         
     @in_specific_channel(channel_name)
     @commands.command()
@@ -23,13 +29,37 @@ class General(commands.Cog):
     async def add(self, ctx: commands.Context, left: int, right: int):
         """Adds two numbers together."""
         await ctx.send(left + right)
-        
+    
     @in_specific_channel(channel_name)
-    @commands.command()
-    async def time(self, ctx: commands.Context):
-        """顯示時間"""
-        time_now = t.strftime("%Y年%m月%d日 %H點%M分", t.localtime())
-        await ctx.send(f'現在時間: {time_now}')
+    async def bonk(self, ctx: commands.Context):
+        # 隨機選一張卡片
+        card = ...
+        
+        # 取得卡片標題和圖片連結
+        title = ...
+        image_link = ...
+        
+        await ctx.send(f'{title}')
+        await ctx.send(f'{image_link}')
+        
+    def dog(self, ):
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        image_link = response.json()['message']
+        return image_link
+        
+    @commands.Cog.listener()
+    async def on_message(self, message: nextcord.Message):
+        author, content = message.author, message.content
+        
+        # check if the message is from channel_name
+        if message.channel.name != channel_name:
+            return
+        
+        if author.name == self.bot.user.name:
+            return
+        
+        if any(x in content for x in ('狗', 'dog')):
+            message = await message.reply(self.dog())
         
 def setup(bot: commands.Bot):
     bot.add_cog(General(bot))
