f5c34562656fa8159f303c5eea2c031fc87a7f15
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-21 23:26:42 +0800

    cp2

--- cogs/general.py
+++ cogs/general.py
@@ -1,7 +1,6 @@
 import nextcord
 from nextcord.ext import commands
 from utils import in_specific_channel
-import requests
 
 channel_name = 'general'
 
@@ -12,36 +11,13 @@ class General(commands.Cog):
     @in_specific_channel(channel_name)
     @commands.command()
     async def Hello(self, ctx: commands.Context):
-        # 加上docstring，可以在help指令中看到
-        """對使用者回覆Hello""" 
-        author = ctx.author.name
-        # await ctx.send(f"Hello, {author}!")
-        await ctx.reply(f"Hello, {author}!")
+        ...
 
-    @in_specific_channel(channel_name)
-    @commands.command()
-    async def add(self, ctx: commands.Context, left: int, right: int):
-        """Adds two numbers together."""
-        await ctx.send(left + right)
-        
-    def dog(self, ):
-        response = requests.get('https://dog.ceo/api/breeds/image/random')
-        image_link = response.json()['message']
-        return image_link
-        
-    @commands.Cog.listener()
-    async def on_message(self, message: nextcord.Message):
-        author, content = message.author, message.content
-        
-        # check if the message is from channel_name
-        if message.channel.name != channel_name:
-            return
-        
-        if author.name == self.bot.user.name:
-            return
-        
-        if any(x in content for x in ('狗', 'dog')):
-            message = await message.reply(self.dog())
+    # add
+    ...
+    
+    # time
+    ...
         
 def setup(bot: commands.Bot):
     bot.add_cog(General(bot))
