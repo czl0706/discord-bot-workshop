b3ed9579ebc338a62a6cbc5fea13320a28e18229
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-22 21:11:30 +0800

    cp2

--- cogs/general.py
+++ cogs/general.py
@@ -7,11 +7,16 @@ channel_name = 'general'
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+    
+    # 範例指令: Hello
     @in_specific_channel(channel_name)
     @commands.command()
     async def Hello(self, ctx: commands.Context):
-        ...
+        # 加上docstring，可以在help指令中看到
+        """對使用者回覆Hello""" 
+        author = ctx.author.name
+        # await ctx.send(f"Hello, {author}!")
+        await ctx.reply(f"Hello, {author}!")
 
     # add
     ...
