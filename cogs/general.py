2a47e7e747223df52eeec3efba27c03d97f53115
Chengzong Li <chengzongli.ee10@nycu.edu.tw>
2024-03-22 21:50:01 +0800

    cp5

--- cogs/general.py
+++ cogs/general.py
@@ -24,12 +24,12 @@ class General(commands.Cog):
         """Adds two numbers together."""
         await ctx.send(left + right)
         
-    @in_specific_channel(channel_name)
-    @commands.command()
-    async def time(self, ctx: commands.Context):
-        """顯示時間"""
-        time_now = t.strftime("%Y年%m月%d日 %H點%M分", t.localtime())
-        await ctx.send(f'現在時間: {time_now}')
+    # @in_specific_channel(channel_name)
+    # @commands.command()
+    # async def time(self, ctx: commands.Context):
+    #     """顯示時間"""
+    #     time_now = t.strftime("%Y年%m月%d日 %H點%M分", t.localtime())
+    #     await ctx.send(f'現在時間: {time_now}')
         
 def setup(bot: commands.Bot):
     bot.add_cog(General(bot))
